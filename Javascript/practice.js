82f282d049ad5013afe5c435d46135e84c362a65
Md.Jahid ul-Islam <145700501+JH-Jahid28@users.noreply.github.com>
2023-10-17 21:57:05 +0600

    upload

---
+++ Javascript/practice.js
@@ -0,0 +1,95 @@
+var rahim = 500;
+var to_buy_apple = 10;
+var to_buy_orange = 8;
+var price_of_apple = 20;
+var price_of_orange = 15;
+
+var apple_cost = to_buy_apple * price_of_apple;
+var orange_cost = to_buy_orange * price_of_orange;
+
+var total_cost = apple_cost + orange_cost;
+
+var back = rahim - total_cost;
+
+console.log(back);
+
+
+
+var number_of_subjects = 4;
+var math = 86;
+var chemistry = 80;
+var physics = 70;
+var bangla = 65;
+
+var total_numbers = math + chemistry + physics + bangla;
+
+var total_avg = total_numbers / number_of_subjects;
+
+console.log(total_avg);
+
+
+
+
+var fname = 'Md Jahidul';
+var lname = 'Islam';
+
+var fulname = fname +' '+ lname;
+
+console.log(fulname);
+
+
+// section-1
+
+var minarina = 7532;
+var minaExtra = 560;
+
+var rinaHave = (minarina - minaExtra)/2;
+var minaHave = (rinaHave + minaExtra);
+
+console.log(rinaHave);
+console.log(minaHave);
+
+// section-2
+
+var fatherdaughterage = 80;
+var guesdaughterage = 1;
+var guesfatherage = 4;
+
+var totalFatherDaughterage = guesdaughterage + guesfatherage;
+
+var daughterage = fatherdaughterage / totalFatherDaughterage;
+var fatherage = daughterage * 4;
+
+console.log(daughterage);
+console.log(fatherage);
+
+
+
+
+// section3
+
+var total_egg = 8;
+var cost_Egg = 72;
+var buy_Egg = 15;
+
+var per_egg_cost = (cost_Egg / total_egg);
+
+var buy_Egg_cost = (per_egg_cost * buy_Egg);
+
+console.log(buy_Egg_cost);
+
+// section-4
+
+var total_have = 3000;
+var prawn_kg = 480;
+var shing_kg = 550;
+var needprawn = 3;
+var needshing = 2;
+
+var prawn_cost = prawn_kg * needprawn;
+var shing_cost = shing_kg * needshing;
+
+var totalCost = shing_cost + prawn_cost;
+
+var totalreturn = total_have - totalCost;
+console.log(totalreturn);
